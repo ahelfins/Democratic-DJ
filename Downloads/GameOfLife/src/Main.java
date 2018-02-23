94202b4f94bb8a139c28b6e0a6c4daaa9ce85689
ashley <ahelfins@macalester.edu>
2018-02-22 20:50:04 -0600

    trying to fix problem

--- Downloads/GameOfLife/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static void main(String[] args){
-		Life game = new Life("life100.txt");
-		//System.out.println(game);
-		game.step(5);
-		System.out.println(game);
-	}
-}
