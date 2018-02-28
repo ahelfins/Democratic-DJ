9fb49e2d3a123263aaa58620e1b4be9d5a8d6eb1
Zachary Skluzacek <zskluzac@macalester.edu>
2018-02-28 13:46:55 -0600

    Added firebase initialization to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -13,6 +14,13 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBH06qobgXssEN8T3DUxrMIHHJnLUdJuOo",
+        authDomain: "democraticdj-4982f.firebaseapp.com",
+        databaseURL: "https://democraticdj-4982f.firebaseio.com",
+        storageBucket: "democraticdj-4982f.appspot.com",
+        messagingSenderId: "395147360380"
+      });
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
