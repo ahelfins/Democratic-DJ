ac39113a8e0828f545c1ab9ed964f06f4ead05db
masaom <mmiazzo@macalester.edu>
2018-03-27 23:23:12 +0100

    got rid of some unnecesary console logs that were slowing down the guest page

--- DemocraticDJ/ddj/src/pages/guest/guest.ts
+++ DemocraticDJ/ddj/src/pages/guest/guest.ts
@@ -49,7 +49,6 @@ export class GuestPage {
       .subscribe(list =>{
         list.forEach(item => {
           //idList.push(item.id)});
-          console.log(item['id']+" pushed to idList");
           //idList.push(item.id);
           this.idList[i] = item['id'];
           i++;
@@ -57,12 +56,10 @@ export class GuestPage {
           // console.log("idList[1] ", this.idList[1]);
           // console.log("idList[2] ", this.idList[2]);
           // console.log("idList[3] ", this.idList[3]);
-          console.log(this.idList);
-
-
         });
         // this.isCorrectRoomInput(this.roomCode, idList);
       });
+    console.log(this.idList);
   }
 
   //getRoomInput() {
