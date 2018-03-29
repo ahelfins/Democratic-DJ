d16beeb2124d9bb486693eae484762094f5ae599
jlim2 <jlim2@macalester.edu>
2018-03-29 00:41:21 -0500

    Clearned up and added documentation/comments for host-song-list.ts

--- DemocraticDJ/ddj/src/pages/host-song-list/host-song-list.ts
+++ DemocraticDJ/ddj/src/pages/host-song-list/host-song-list.ts
@@ -8,7 +8,9 @@ import { SessionDataProvider } from "../../providers/session-data/session-data";
 
 
 /**
- * Generated class for the HostSongListPage page.
+ * Generated class for the HostSongListPage page. Displays the room-specific
+ * song list for Host (Identical to Guest's but has a functionality to add
+ * songs to Spotify Queue.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -22,20 +24,16 @@ import { SessionDataProvider } from "../../providers/session-data/session-data";
 export class HostSongListPage {
   addSongButton: any;
   roomId: string;
-  public songName: AddSongPage;
-  songList: AngularFireList<any>;
+  // public songName: AddSongPage;
+  // songList: AngularFireList<any>;
   song: Observable<any[]>;
   title: String;
 
   constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    public afDB: AngularFireDatabase,
     public fBProvider: FirebaseProvider,
     private sDProvider: SessionDataProvider) {
       this.addSongButton = AddSongPage;
-      this.roomId = this.sDProvider.getRoomCode();
-
-
+      this.roomId = this.sDProvider.getRoomCode();  //Gets the roomId from the Session Data Provider
     //this.songList = fBProvider.getSongList(this.roomId);
     //this.song = this.songList.valueChanges();
   }
@@ -43,11 +41,8 @@ export class HostSongListPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad HostSongListPage');
     console.log('Current room: ' +this.roomId);
-    // document.getElementById('room').textContent = "Room: "+this.roomId;
-    // document.getElementById('room').textContent = "Room: "+ this.sDProvider.getRoomCode();
-    this.title = "Host: "+ this.roomId
+    this.title = "Host: "+ this.roomId;
     //this.songList.valueChanges();
-
 
     // let i = 0;
     // this.afDB.list("/songs").valueChanges()
@@ -65,15 +60,26 @@ export class HostSongListPage {
     // console.log("songList: " + this.songList);
   }
 
+  /**
+   * Takes an user to the AddSongPage of the specified room.
+   */
   goToAddSongPage() {
     console.log("roomId going to add song page:" + this.roomId)
     this.navCtrl.push(AddSongPage, {roomId: this.roomId});
   }
 
+  /**
+   * Adds a song to the Queue that integrates with Spotify
+   * @param song
+   */
   addToQueue(song) {
     this.fBProvider.pushSong(song, this.roomId);
   }
 
+  /**
+   * Deletes a song from the list (and the Firebase)
+   * @param song
+   */
   delete(song) {
 
   }
