a43828a0e2769b539737273471fbbc9a83610917
Zachary Skluzacek <zskluzac@macalester.edu>
2018-04-19 21:26:49 -0500

    Song Cleansing

--- DemocraticDJ/ddj/src/pages/add-song/add-song.ts
+++ DemocraticDJ/ddj/src/pages/add-song/add-song.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { GuestSongListPage } from '../guest-song-list/guest-song-list';
 import { HostSongListPage} from '../host-song-list/host-song-list';
@@ -33,6 +33,7 @@ export class AddSongPage {
   //songList : AngularFireList<any>
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
+              public alertCtrl: AlertController,
               public fBProvider: FirebaseProvider,
               private sDProvider: SessionDataProvider) {
 
@@ -48,17 +49,62 @@ export class AddSongPage {
     console.log('Add Song Room: ' + this.roomId); // DEBUG
   }
 
+  isValidInput(song:string, artist:string) {
+    if(song != null || artist != null) {
+      if(song == null) {
+        song = "1";
+      }
+      if(artist == null) {
+        artist = "1";
+      }
+      if(song.length < 21 && artist.length <21) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  cleanArtist(artist:string) {
+    if(artist == null) {
+      artist = "Unknown";
+    }
+    return artist;
+  }
+
+  cleanSong(song: string) {
+    if(song == null) {
+      song = "Any Song";
+    }
+    return song;
+  }
+
+  badSongAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Invalid Input',
+      message: 'It looks like your song choice was either too long or wasn\'t a valid input. Please try again!',
+      buttons: ['Okay']
+    });
+    alert.present();
+  }
+
 
   /**
    * Adds the song the user input to the Firebase under the specific room
    * where the user is located.
    * @param songInput - user input of the song name
    */
-  addSongToFB(songInput) {
-    console.log("songName: " + songInput + ", and roomCode: " +this.roomId); // DEBUG
-    let song: Song = {title: songInput, upVotes: 0, downVotes: 0}; // converts the song to a Song object
-    this.fBProvider.pushSong(song, this.roomId); // pushes the song to the Firebase
-    console.log("fbkey "+ song.fbKey);
+  addSongToFB(songInput, artistInput) {
+    if(this.isValidInput(songInput, artistInput)) {
+      console.log("songName: " + songInput + ", and roomCode: " +this.roomId); // DEBUG
+      let song: Song = {title: this.cleanSong(songInput), artist: this.cleanArtist(artistInput), upVotes: 0, downVotes: 0}; // converts the song to a Song object
+      this.fBProvider.pushSong(song, this.roomId); // pushes the song to the Firebase
+      console.log("fbkey "+ song.fbKey);
+    } else {
+      this.badSongAlert();
+    }
   }
 
   /**
@@ -92,12 +138,5 @@ export class AddSongPage {
   //   )
   // }
 
-
-
-  // getSong() {
-  //   this.songName = document.getElementsByName('songName').item;
-  //   return this.songName;
-  //
-  // }
 
 }
